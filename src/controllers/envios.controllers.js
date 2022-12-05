87ee6f1bc80751082510dc67e42e4f9e9b18860f
Humberto Barrera <humbertobarcam@gmail.com>
2022-12-05 12:59:40 -0700

    envios

--- src/controllers/envios.controllers.js
+++ src/controllers/envios.controllers.js
@@ -2,15 +2,15 @@ import {pool} from "../db.js"
 
 export const solicTrans = async (req, res) => {
     try{
-        const date = new Date()
+        const date = new Date();
         const [row] = await pool.query("SELECT O.idPedido as 'NoVta', PR.nombre as 'Producto', CONCAT(DIR.ciudad, ', ', DIR.estado) as 'NombreDest',\n"+
                     "CONCAT(DIR.calle, ' #', DIR.numeroExt, ' Colonia ', DIR.colonia) as 'DirDest', DIR.comentarios as 'Instrucciones de entrega' FROM orden O \n"+
                     "INNER JOIN producto PR ON O.idProducto = PR.idProducto\n"+
                     "INNER JOIN pedido PE ON O.idPedido = PE.idPedido\n"+
                     "INNER JOIN direccion DIR ON DIR.idCliente = PE.idCliente\n"+
-                    "WHERE PE.idPedido = ?", [req.params.id])
-        const info = {"DirOrig": "Dirección de prueba", "fechaEntr": date}
-        Object.assign(row[0], info)
+                    "WHERE PE.idPedido = ?", [req.params.id]);
+        const info = {"DirOrig": "Dirección de prueba", "fechaEntr": date};
+        Object.assign(row[0], info);
         if (row.length <= 0) return res.status(404).json({
             message: 'Algo salió mal. No se pudo realizar la solicitud de transporte.'
         })
@@ -18,6 +18,24 @@ export const solicTrans = async (req, res) => {
     }catch (error){
         return res.status(500).json({
             message: 'Algo salió mal. No se pudo realizar la solicitud de transporte.'
+        })
+    }
+}
+
+
+export const estTransp = async (req, res) => {
+    try{
+        const {edoEntr, NoGuia, NoVta, fecha} = req.body
+        const [row_transp] = await pool.query('INSERT INTO envio (claveRastreo, fechaEntrega, estadoEnvio, idPedido) VALUES (?, ?, ?, ?)',
+                                                [NoGuia, fecha, edoEntr, NoVta]);
+        res.json({
+            message: 'Entrega registrada con éxito',
+            estadoEntrega: edoEntr,
+            fechaEntrega: fecha
+        })
+    }catch (error){
+        return res.status(500).json({
+            message: 'Algo salió mal. No se pudo recibir el estado de transporte.'
         })
     }
 }
--- src/routes/envios.router.js
+++ src/routes/envios.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { solicTrans } from "../controllers/envios.controllers.js";
+import { solicTrans, estTransp } from "../controllers/envios.controllers.js";
 
 const router = Router();
 
 router.get('/solicTrans/:id', solicTrans);
+
+router.post('/estado_transporte', estTransp);
 
 export default router
