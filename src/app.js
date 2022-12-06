5b5e20ab3c15279cabbe3cfec09dd77c499af5d9
Humberto Barrera <humbertobarcam@gmail.com>
2022-12-06 10:29:02 -0700

    added app.js

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express"
+import clientesRoutes from './routes/clientes.routes.js'
+import productosRoutes from './routes/productos.routers.js'
+import metodopagoRoutes from './routes/metodopago.routes.js'
+import pedidosRoutes from './routes/pedidos.routes.js'
+import ventasRoutes from './routes/ventas.routes.js'
+import enviosRoutes from './routes/envios.router.js'
+import pingRoute from './routes/ping.routes.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(pingRoute)
+app.use('/api', clientesRoutes)
+app.use('/api', metodopagoRoutes)
+app.use('/api', productosRoutes)
+app.use('/api', pedidosRoutes)
+app.use('/api', ventasRoutes)
+app.use('/api', enviosRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'API Not found'
+    })
+})
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
-import express from "express"
-import clientesRoutes from './routes/clientes.routes.js'
-import productosRoutes from './routes/productos.routers.js'
-import metodopagoRoutes from './routes/metodopago.routes.js'
-import pedidosRoutes from './routes/pedidos.routes.js'
-import ventasRoutes from './routes/ventas.routes.js'
-import enviosRoutes from './routes/envios.router.js'
-import pingRoute from './routes/ping.routes.js'
-
+import app from './app.js'
 import {PORT} from './config.js'
 
-const app = express()
-
-app.use(express.json())
-
-app.use(pingRoute)
-app.use('/api', clientesRoutes)
-app.use('/api', metodopagoRoutes)
-app.use('/api', productosRoutes)
-app.use('/api', pedidosRoutes)
-app.use('/api', ventasRoutes)
-app.use('/api', enviosRoutes)
-
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'API Not found'
-    })
-})
-
-app.listen(3000)
+app.listen(PORT)
 console.log("Server running on port", PORT)
